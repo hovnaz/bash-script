974197640eedbdd72bba9ed46fe18dcfd6b6594c
Hovhannes <43682600+hovnaz@users.noreply.github.com>
2023-08-09 21:31:40 +0400

    Update continuous_block_ip.sh

--- continuous_block_ip.sh
+++ continuous_block_ip.sh
@@ -1,4 +1,25 @@
-  GNU nano 6.2                            continuous_block_ip.sh                                      #!/bin/bash
+#!/bin/bash
+
+# # Set the threshold for failed login attempts
+# THRESHOLD=3
+
+# # Function to block an IP address
+# block_ip() {
+#     local ip_address="$1"
+#     if ! iptables -C INPUT -s "$ip_address" -j DROP &> /dev/null; then
+#         iptables -A INPUT -s "$ip_address" -j DROP
+#         echo "Blocked IP address $ip_address"
+#     fi
+# }
+
+# # Parse the auth.log file to find IP addresses with failed attempts exceeding the threshold
+# grep "Failed password for" /var/log/auth.log | awk '{print $(NF-3)}' | sort | uniq -c | while read co>    if [ "$count" -ge "$THRESHOLD" ]; then
+#         block_ip "$ip_address"
+#     fi
+# done
+
+
+#!/bin/bash
 
 # Set the threshold for failed login attempts
 THRESHOLD=3
@@ -12,8 +33,15 @@ block_ip() {
     fi
 }
 
-# Parse the auth.log file to find IP addresses with failed attempts exceeding the threshold
-grep "Failed password for" /var/log/auth.log | awk '{print $(NF-3)}' | sort | uniq -c | while read co>    if [ "$count" -ge "$THRESHOLD" ]; then
-        block_ip "$ip_address"
+# Continuously monitor the auth.log file for incoming SSH failed login attempts
+tail -n0 -F /var/log/auth.log | while read line; do
+    if [[ "$line" == *"Failed password for"* ]]; then
+        ip_address=$(echo "$line" | awk '{print $(NF-3)}')
+        if [ -n "$ip_address" ]; then
+            failed_attempts=$(grep -c "Failed password for" /var/log/auth.log | grep "$ip_address")
+            if [ "$failed_attempts" -ge "$THRESHOLD" ]; then
+                block_ip "$ip_address"
+            fi
+        fi
     fi
 done
